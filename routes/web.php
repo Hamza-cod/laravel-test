1d868cbfa75f04d5383e0a0afe7796f2605d7c9c
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2024-03-13 16:38:40 +0100

    add last modification

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+Route::resource('product', ProductController::class)->only(['show', 'index']);
+Route::resource('category', CategoryController::class)->only(['show', 'index']);
+
 Route::group(['middleware' => ["auth:sanctum"]],function ()  {
-    Route::resource('product',ProductController::class);
-    Route::resource('category',CategoryController::class);
+    Route::resource('product',ProductController::class)->except(['show', 'index']);
+    Route::resource('category',CategoryController::class)->except(['show', 'index']);
 });
 
 
